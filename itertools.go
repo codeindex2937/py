8d3c420d947767af07be43a148ee77f66ad3146d
hh <hh@b.c>
2023-03-27 00:20:30 +0800

    feat: add more iter functions

--- itertools.go
+++ itertools.go
@@ -83,3 +83,123 @@ func Sorted[K constraints.Ordered](ks []K) []K {
 	slices.Sort(clone)
 	return clone
 }
+
+func Repeat[K any](k K, times int) []K {
+	result := []K{}
+	for i := 0; i < times; i++ {
+		result = append(result, k)
+	}
+	return result
+}
+
+func Range[K numeric](start, stop, step K) []K {
+	result := []K{}
+	for k := start; k < stop; {
+		result = append(result, k)
+	}
+	return result
+}
+
+func Combination[K any](slice []K, k int) [][]K {
+	if k < 0 || k > len(slice) {
+		return [][]K{}
+	}
+
+	index := make([]int, k)
+	for i := 0; i < k; i++ {
+		index[i] = i
+	}
+
+	var result [][]K
+
+	for {
+		comb := make([]K, k)
+		for i, j := range index {
+			comb[i] = slice[j]
+		}
+		result = append(result, comb)
+
+		i := k - 1
+		for ; i >= 0; i-- {
+			if index[i]+1 != len(slice)-(k-i-1) {
+				break
+			}
+		}
+
+		if i < 0 {
+			break
+		}
+
+		index[i]++
+		for j := i + 1; j < k; j++ {
+			index[j] = index[j-1] + 1
+		}
+	}
+
+	return result
+}
+
+func Product[K any](slices ...[]K) [][]K {
+	result := [][]K{}
+	for _, s := range slices {
+		next := [][]K{}
+		for _, v := range s {
+			for _, r := range result {
+				next = append(next, append(r, v))
+			}
+		}
+		result = next
+	}
+	return result
+}
+
+func GroupBy[K any, V comparable](slice []K, vFn func(k K) V) map[V][]K {
+	result := map[V][]K{}
+	for _, k := range slice {
+		g := vFn(k)
+		if vs, ok := result[g]; ok {
+			result[g] = append(vs, k)
+		} else {
+			result[g] = []K{k}
+		}
+	}
+	return result
+}
+
+func Zip[K any](slices ...[]K) [][]K {
+	result := [][]K{}
+	if len(slices) < 1 {
+		return result
+	}
+
+	minLen, _ := Min(MapSlice(func(slice []K) int { return len(slice) }, slices...)...)
+	for i := 0; i < minLen; i++ {
+		r := []K{}
+		for _, slice := range slices {
+			r = append(r, slice[i])
+		}
+		result = append(result, r)
+	}
+	return result
+}
+
+func ZipLongest[K any](k K, slices ...[]K) [][]K {
+	result := [][]K{}
+	if len(slices) < 1 {
+		return result
+	}
+
+	maxLen, _ := Max(MapSlice(func(slice []K) int { return len(slice) }, slices...)...)
+	for i := 0; i < maxLen; i++ {
+		r := []K{}
+		for _, slice := range slices {
+			if len(slice) <= i {
+				r = append(r, k)
+			} else {
+				r = append(r, slice[i])
+			}
+		}
+		result = append(result, r)
+	}
+	return result
+}
